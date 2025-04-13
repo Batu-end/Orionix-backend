e928e3fc18ccfe0cdb0c64550b7475830e2e6499
Vladyslav Korenevskyi <25588519+tmbkoren@users.noreply.github.com>
2025-04-13 10:02:32 -0700

    added cors policy

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from app.weather import get_with_zip, get_with_coor, get_weather_condition
 from app.events import get_events_future
+from app.planner import generate_tonight_summary
 
 env_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.env'))
 load_dotenv(dotenv_path=env_path)
@@ -10,6 +12,19 @@ load_dotenv(dotenv_path=env_path)
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8081",  # Replace with your React Native development server URL  # If your backend is on this origin
+    # Add other origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # weather, date, location, event type are
 
@@ -41,7 +56,15 @@ async def future_events():
     return res
 
 
-@app.get("/weatherType")
+@app.get("/weatherTypeByCoordinates")
 async def weather_type(lat, lon):
-    res = await get_weather_condition(lat, lon)
+    res = await generate_tonight_summary(lat, lon)
+    return res
+
+@app.get('/weatherTypeByZip')
+async def weather_type_by_zip(zip_code):
+    print(zip_code)
+    lat, lon = await get_with_zip(zip_code)
+    res = await generate_tonight_summary(lat, lon)
+    print(res)
     return res
