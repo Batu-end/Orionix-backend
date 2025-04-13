68f9775ccd09a7589bda1877dd2da740bd294d90
Batu Demirtas <batu@batu-362.local>
2025-04-12 18:55:02 -0700

    File structure, venv and OpenWeather API key.

---
+++ app/services/weather.py
@@ -0,0 +1,15 @@
+
+
+def get_weather(lat, lon):
+    """
+    Get the weather for a given latitude and longitude.
+
+    Args:
+        lat (float): Latitude.
+        lon (float): Longitude.
+
+    Returns:
+
+    """
+
+    https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}
