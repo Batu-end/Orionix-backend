25a22c4c6dd4d6ef6a452a5b10a65a02ddcec15b
Batu Demirtas <batu@batu-362.local>
2025-04-12 22:14:30 -0700

    api call route WIP

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI, Query
 import httpx
 from dotenv import load_dotenv
+from ..services.weather import get_with_zip, get_with_coor
 
 env_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '.env'))
 load_dotenv(dotenv_path=env_path)
@@ -13,13 +14,20 @@ app = FastAPI()
 # weather, date, location, event type are
 
 
-@app.get("/getEventsByCoordinates") # route identification
+@app.post("/getEventsByCoordinates") # route identification
 async def get_event_by_coor(lat, lon):
     # get lat, lon from allow
     # return json
+    await get_with_coor(lat, lon)
     print(lat, lon)
 
+    return
 
-@app.get("/getEventsByZip")
+
+@app.post("/getEventsByZip")
 async def get_event_by_zip(zip_code):
+
+    lat, lon = await get_with_zip(zip_code)
+
+    await get_with_coor(lat, lon)
     print(zip_code)
--- app/services/weather.py
+++ app/services/weather.py
@@ -10,24 +10,27 @@ API_KEY = os.getenv("OPENWEATHER_API_KEY")
 # OPENWEATHER API CALL: https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}
 # GEOCODER API CALL: http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
 
-async def get_coordinates(city: str = None, zip_code: str = None, country: str = "US"):
-
-    async def get_lat_lon(city: str, country: str):
-
-        async with httpx.AsyncClient() as client:
-
-            if zip_code:
-                api_url_zip = f"http://api.openweathermap.org/geo/1.0/zip?zip={zip_code},{country}&appid={API_KEY}"
-                zip_response = await client.get(api_url_zip)
-                # if zip_response.status_code == 200:
-                data = zip_response.json()
-                    
-        lat = data.get("lat")
-        lon = data.get("lon")
-        return lat, lon
+# async def get_coordinates(city: str = None, zip_code: str = None, country: str = "US"):
 
 
-async def get_weather(lat: float, lon: float):
+async def get_with_zip(zip_code: str):
+
+    country = "US"
+    async with httpx.AsyncClient() as client:
+
+        if zip_code:
+            api_url_zip = f"http://api.openweathermap.org/geo/1.0/zip?zip={zip_code},{country}&appid={API_KEY}"
+            zip_response = await client.get(api_url_zip)
+            # if zip_response.status_code == 200:
+            data = zip_response.json()
+
+    lat = data.get("lat")
+    lon = data.get("lon")
+
+    return lat, lon
+
+
+async def get_with_coor(lat: float, lon: float):
 
     async with httpx.AsyncClient() as client:
 
